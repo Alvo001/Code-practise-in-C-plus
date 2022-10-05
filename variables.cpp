b261ee434902e063d3ee8744f068b2e8eba8c4a1
Lema05 <lemakipeno@gmail.com>
2022-10-05 09:48:00 +0300

    variables in C++

---
+++ variables.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main(){
+
+//integer (whole number)
+
+int age = 21;
+int year = 2021;
+int days = 7.5;
+
+//double (number including decimal)
+
+double price = 10.99;
+double gpa = 2.5;
+double temperature = 25.1;
+
+// char (single character)
+
+char grade = 'A';
+char initial = 'B';
+char currency = '$';
+
+//boolean (true or false)
+bool student = false;
+bool power = true;
+bool forSale = true;
+
+//string (object that represents a sequence of text)
+std::string name = "Bro";
+std::string day = "Friday";
+std::string food = "Pizza";
+std::string address = "123 Lake Tahoe";
+
+std:: cout << "Hello " <<name << '\n';
+std:: cout << "You are " <<age << "years old" <<'\n';
+
+    return 0;
+}
