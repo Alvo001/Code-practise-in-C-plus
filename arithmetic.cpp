6730320c9bcdb20d7714de6207e0dd8e55a789fe
Lema05 <lemakipeno@gmail.com>
2022-10-05 10:54:21 +0300

    Arithmetic operators in C++

---
+++ arithmetic.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+
+int main (){
+    
+
+    double students = 20;
+
+    //addition
+    // students++;
+    // students+=1;
+
+    //subtraction
+    students = students -2;
+    students-=2;
+    students--;
+ 
+    //multiplication2
+    students = students * 2;
+    students *=2;
+
+ 
+
+    std::cout <<students;
+    return 0;
+
+}
+
+
+
+
