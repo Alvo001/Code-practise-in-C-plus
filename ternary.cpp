b978ff56a21aa1288ee76d838c364456cec67e0e
Lema05 <lemakipeno@gmail.com>
2022-10-05 12:01:53 +0300

    ternary operation in c++

---
+++ ternary.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+int main()
+{
+ int grade = 75;
+ grade >= 60? std::cout <<"You pass!":
+
+std::cout << "You fail!";
+
+
+int number = 9;
+number % 2 == 1? std::cout <<"ODD" : std::cout <<"EVEN";
+
+bool hungry = true;
+
+hungry? std::cout <<"You are hungry": std::cout <<"you are full!";
+
+
+
+return 0;
+
+}
+
