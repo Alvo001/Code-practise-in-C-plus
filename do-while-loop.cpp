7a506b71ce43b1752863673e19b8d3b40f62e509
Lema05 <lemakipeno@gmail.com>
2022-10-05 12:51:46 +0300

    do while loop in C++

---
+++ do-while-loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+
+//do while loop = do some block of code first, THEN repeat again if the condition is true
+ 
+
+    
+   do{
+        std::cout<<"Enter a positive #: ";
+        std::cin >> number;
+    } while(number <0);
+
+    std::cout << "The # is: "<<number;
+
+
+
+    return 0;
+}
