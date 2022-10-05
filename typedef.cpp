dd9839490ed7afde832058147887fa06d8bbd30f
Lema05 <lemakipeno@gmail.com>
2022-10-05 10:18:47 +0300

    typedef in C++ - makes it easier to read code

---
+++ typedef.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <vector>
+
+typedef std::string text_t; //using can be used instead of typedef
+typedef int number_t;
+
+int main (){
+    text_t firstName = "Bro";
+    number_t age = 21;
+
+    std::cout <<firstName<<'\n';
+    std::cout <<age<<'\n';
+    return 0;
+
+}
+
+
+
+
